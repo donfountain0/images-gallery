d370f1c18b73098aab422f8b9f9a19f37decbd9c
Don Fountain <donfountain0@gmail.com>
2023-05-31 19:10:53 -0400

    made submit button log to console

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
+const handleSearchSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target[0].value)
+}
 
 const App = () => {
+  const [word, setWord] = useState('')
   return (
     <div>
-      <Header></Header>
+      <Header title="Images Gallery"> </Header>
+      <Search handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
+
+const navbarStyle = {
+    backgroundColor:'lightblue'
+}
+
 
 const Header = () => {
     return (
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+        <Navbar style={navbarStyle} variant="light">
+            <Container>
+                <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+            </Container>
         </Navbar>
     )
 };
